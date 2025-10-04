db2105dd9c7d695778fa9cd612f0ed1ebe173ec0
Vrypt <keriptozzzzz@gmail.com>
2025-10-04 13:55:14 +0700

    update: plugin source

--- plugins/utility/script.js
+++ plugins/utility/script.js
@@ -4,17 +4,17 @@ export default {
     command: ["sc", "script"],
     run: async (conn, m, { Func }) => {
         try {
-            const res = await Func.fetchJson("https://api.github.com/repos/AgusXzz/ESEMPE-MD");
+            const res = await Func.fetchJson("https://api.github.com/repos/VryptLab/EternityBot");
 
             m.reply(`*Informasi Script*\n
-✨ *Nama:* ${res.name}
-👤 *Pemilik:* ${res.owner.login ?? "-"}
-⭐ *Star:* ${res.stargazers_count ?? 0}
-🍴 *Forks:* ${res.forks ?? 0}
-📅 *Dibuat sejak:* ${Func.ago(res.created_at)}
-♻️ *Terakhir update:* ${Func.ago(res.updated_at)}
-🚀 *Terakhir publish:* ${Func.ago(res.pushed_at)}
-🔗 *Link:* ${res.html_url}
+◦ *Nama:* ${res.name}
+◦ *Pemilik:* ${res.owner.login ?? "-"}
+◦ *Star:* ${res.stargazers_count ?? 0}
+◦ *Forks:* ${res.forks ?? 0}
+◦ *Dibuat sejak:* ${Func.ago(res.created_at)}
+◦️ *Terakhir update:* ${Func.ago(res.updated_at)}
+◦ *Terakhir publish:* ${Func.ago(res.pushed_at)}
+◦ *Link:* ${res.html_url}
 `);
 
         } catch (err) {
